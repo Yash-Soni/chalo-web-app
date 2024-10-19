fc6eebf650379f87228cbc9371b0de9a211f49d3
Yash Soni <yashjpsoni264@gmail.com>
2024-10-19 16:43:26 +0530

    added stops for the routes and few dependencies to get started

---
+++ chalo-web/src/data/stops.js
@@ -0,0 +1,72 @@
+export const STOPS = [
+  {
+    id: 1,
+    label: 'Mumbai',
+    value: 'Mumbai',
+    lat: 19.07609,
+    lng: 72.877426,
+  },
+  {
+    id: 2,
+    label: 'Indore',
+    value: 'Indore',
+    lat: 22.71692,
+    lng: 75.85970
+  },
+  {
+    id: 3,
+    label: 'Bacchan Dhaba',
+    value: 'Bacchan Dhaba',
+    lat: 22.87264,
+    lng: 75.96445,
+  },
+  {
+    id: 4,
+    label: 'Radisson Blu Indore',
+    value: 'Radisson',
+    lat: 22.75093,
+    lng: 75.90530
+  },
+  {
+    id: 5,
+    label: 'Vijay Nagar Square',
+    value: 'Vijay-Nagar',
+    lat: 22.751020,
+    lng: 75.895468
+  },
+  {
+    id: 6,
+    label: 'Airport',
+    value: 'Airport',
+    lat: 22.72947,
+    lng: 75.80441
+  },
+  {
+    id: 7,
+    label: 'Rajwada',
+    value: 'Rajwada',
+    lat: 22.71875,
+    lng: 75.85512
+  },
+  {
+    id: 8,
+    label: 'Janjeerwala Square',
+    value: 'Janjeerwala Square',
+    lat: 22.726571,
+    lng: 75.880980
+  },
+  {
+    id: 9,
+    label: 'Khajrana Square',
+    value: 'Khajrana Square',
+    lat: 22.731729,
+    lng: 75.902277
+  },
+  {
+    id: 10,
+    label: 'Malwa Mill Square',
+    value: 'Malwa Mill',
+    lat: 22.734186,
+    lng: 75.875093
+  }
+]
